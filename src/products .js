d7c1354e3129d7f8a29aa929aed3d62dbb101405
umeshnarayancedcoss <umeshnarayan@cedcoss.com>
2022-10-21 17:30:13 +0530

    jQuery Filter

---
+++ src/products .js
@@ -0,0 +1,36 @@
+var products = [{
+					"id": "100",
+					"name": "iPhone 4S",
+					"brand": "Apple",
+					"os": "iOS"
+				},
+				{
+					"id": "101",
+					"name": "Moto X",
+					"brand": "Motorola",
+					"os": "Android"	
+				},
+				{
+					"id": "102",
+					"name": "iPhone 6",
+					"brand": "Apple",
+					"os": "iOS"
+				},
+				{
+					"id": "103",
+					"name": "Samsung Galaxy S",
+					"brand": "Samsung",
+					"os": "Android"
+				},
+				{
+					"id": "104",
+					"name": "Google Nexus",
+					"brand": "ASUS",
+					"os": "Android"
+				},
+				{
+					"id": "105",
+					"name": "Surface",
+					"brand": "Microsoft",
+					"os": "Windows"
+				}];
